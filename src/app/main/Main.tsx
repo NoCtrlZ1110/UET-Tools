9bca68309a5201f203472fa49cb20cc7ba33cc2b
NoCtrlZ1110 <huynd.nguyen@gmail.com>
2021-04-06 17:41:22 +0700

    add universities

--- src/app/main/Main.tsx
+++ src/app/main/Main.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { universities } from '../../const/Universities';
 
 const Main: React.FC = () => {
-  return <div className='main'>Content here!</div>;
+  return (
+    <div className='main'>
+      {universities.map((u) => (
+        <div className='university d-flex'>
+          <img
+            src={`/logo/${u.acronym}.png`}
+            alt={`logo-${u.acronym}`}
+            width={50}
+          />
+          <div className='university-name'>{u.name}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Main;
---
+++ src/const/Universities.ts
@@ -0,0 +1,15 @@
+interface University {
+  name: string;
+  acronym: string;
+}
+
+export const universities: Array<University> = [
+  {
+    name: 'Trường Đại học Công nghệ',
+    acronym: 'UET',
+  },
+  {
+    name: 'Trường Đại học Ngoại ngữ',
+    acronym: 'ULIS',
+  },
+];
